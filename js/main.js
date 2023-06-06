dbbcbb0b57ba6cf15029183ce55a8b6175a9e79f
nocapkenny <nocapkenny@yandex.ru>
2023-06-06 23:44:05 +0300

    1.0

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.bike-slider, .slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: 2000,
+    })
+    $(".menu,.bike__column").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;	
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
